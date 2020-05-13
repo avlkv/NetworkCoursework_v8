a21576e825579bef76dad8fbcde8a77537666e7a
Artyom Volkov <43137048+avlkv@users.noreply.github.com>
2020-05-13 12:56:31 +0300

    Add files via upload

---
+++ ComCom/ComForm/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace ComForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.b_con = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.cb_PortNames = new System.Windows.Forms.ComboBox();
+            this.b_OpenPort = new System.Windows.Forms.Button();
+            this.b_ChooseFile = new System.Windows.Forms.Button();
+            this.b_SendFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(21, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(512, 380);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(557, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send \"Hello\" as msg";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // b_con
+            // 
+            this.b_con.Location = new System.Drawing.Point(557, 248);
+            this.b_con.Name = "b_con";
+            this.b_con.Size = new System.Drawing.Size(141, 23);
+            this.b_con.TabIndex = 4;
+            this.b_con.Text = "Проверить соединение";
+            this.b_con.UseVisualStyleBackColor = true;
+            this.b_con.Click += new System.EventHandler(this.b_con_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(557, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Send \"Hello\" as file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // cb_PortNames
+            // 
+            this.cb_PortNames.FormattingEnabled = true;
+            this.cb_PortNames.Location = new System.Drawing.Point(557, 191);
+            this.cb_PortNames.Name = "cb_PortNames";
+            this.cb_PortNames.Size = new System.Drawing.Size(141, 21);
+            this.cb_PortNames.TabIndex = 6;
+            // 
+            // b_OpenPort
+            // 
+            this.b_OpenPort.Location = new System.Drawing.Point(557, 219);
+            this.b_OpenPort.Name = "b_OpenPort";
+            this.b_OpenPort.Size = new System.Drawing.Size(141, 23);
+            this.b_OpenPort.TabIndex = 7;
+            this.b_OpenPort.Text = "Открыть порт";
+            this.b_OpenPort.UseVisualStyleBackColor = true;
+            this.b_OpenPort.Click += new System.EventHandler(this.b_OpenPort_Click);
+            // 
+            // b_ChooseFile
+            // 
+            this.b_ChooseFile.Location = new System.Drawing.Point(557, 296);
+            this.b_ChooseFile.Name = "b_ChooseFile";
+            this.b_ChooseFile.Size = new System.Drawing.Size(141, 23);
+            this.b_ChooseFile.TabIndex = 8;
+            this.b_ChooseFile.Text = "Выбрать файл...";
+            this.b_ChooseFile.UseVisualStyleBackColor = true;
+            this.b_ChooseFile.Click += new System.EventHandler(this.b_ChooseFile_Click);
+            // 
+            // b_SendFile
+            // 
+            this.b_SendFile.Location = new System.Drawing.Point(557, 326);
+            this.b_SendFile.Name = "b_SendFile";
+            this.b_SendFile.Size = new System.Drawing.Size(141, 23);
+            this.b_SendFile.TabIndex = 9;
+            this.b_SendFile.Text = "Отправить файл";
+            this.b_SendFile.UseVisualStyleBackColor = true;
+            this.b_SendFile.Click += new System.EventHandler(this.b_SendFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(557, 356);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(557, 13);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(141, 23);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 413);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b_SendFile);
+            this.Controls.Add(this.b_ChooseFile);
+            this.Controls.Add(this.b_OpenPort);
+            this.Controls.Add(this.cb_PortNames);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.b_con);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "COMpass";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button b_con;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox cb_PortNames;
+        private System.Windows.Forms.Button b_OpenPort;
+        private System.Windows.Forms.Button b_ChooseFile;
+        public System.Windows.Forms.Button b_SendFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ ComCom/ComForm/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComForm
+{
+    
+    public partial class Form1 : Form
+    {
+        Connection com1 = new Connection();
+        //Connection com2 = new Connection();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            cb_PortNames.Items.AddRange(SerialPort.GetPortNames());
+            com1.b_SendFile = b_SendFile;
+            com1.MainForm = this;
+            com1.Display = richTextBox1;
+            com1.TestLabel = label1;
+            com1.ProgressBar = progressBar1;
+        }
+
+        //private void b_open5_Click(object sender, EventArgs e)
+        //{
+        //    com1.Log = richTextBox1;
+        //    com1.setPortName("COM1"); //давать список для выбора из существующих?
+        //    com1.OpenPort();
+            
+        //    richTextBox1.AppendText(com1.Port.PortName + "  is opened: " + com1.Port.IsOpen + "\n");
+        //}
+
+        //private void b_open6_Click(object sender, EventArgs e)
+        //{
+        //    com2.Log = richTextBox1;
+        //    com2.setPortName("COM2");
+        //    com2.OpenPort();
+            
+        //    richTextBox1.AppendText(com2.Port.PortName + "  is opened: " + com2.Port.IsOpen + "\n");
+        //}
+
+        /// <summary>
+        /// Пишет в лог, есть ли соединение
+        /// </summary>
+        private void b_con_Click(object sender, EventArgs e)
+        {
+            richTextBox1.AppendText(cb_PortNames.SelectedItem.ToString() + " is connected: " + com1.IsConnected() + "\n");
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (com1.IsConnected())
+            {
+                com1.WriteData("Hello world", Connection.FrameType.MSG);
+            }
+            else
+            {
+                richTextBox1.AppendText("error: no connection\n");
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (com1.IsConnected())
+            {
+                com1.WriteData("filebytesbytesbytes", Connection.FrameType.FILE);
+            }
+            else
+            {
+                richTextBox1.AppendText("error: no connection\n");
+            }
+        }
+
+        /// <summary>
+        /// Открывает порт com1
+        /// </summary>
+        private void b_OpenPort_Click(object sender, EventArgs e)
+        {
+            com1.Log = richTextBox1;
+            com1.setPortName(cb_PortNames.SelectedItem.ToString()); //давать список для выбора из существующих?
+            com1.OpenPort();
+            //com1.Port.Bu
+            richTextBox1.AppendText(com1.Port.PortName + "  is opened: " + com1.Port.IsOpen + "\n");
+        }
+
+        private void b_ChooseFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                com1.WriteData(openFileDialog.FileName, Connection.FrameType.FILEOK);
+                //if (_com1.loading.i == 2)
+                //{
+                //    richTextBox1.AppendText("[" + DateTime.Now + "] Вы: файл отправлен" + "\n");
+                //}
+                richTextBox1.ScrollToCaret();
+                //if (_com1.loading.i == 3)
+                //{
+                //    richTextBox2.AppendText("Передача файла отменена. \n");
+                //}
+            }
+        }
+
+        private void b_SendFile_Click(object sender, EventArgs e)
+        {
+            b_SendFile.DialogResult = DialogResult.OK;
+        }
+    }
+}
